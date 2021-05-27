f5958759903d41260e6f5fb3a72cbc5e62d10805
Desmond Tuiyot <dtuiyot@gmail.com>
2021-05-27 00:49:04 -0400

    code to include heroku hosted server when in production

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,10 @@
 import axios from "axios";
 
+let url = "";
+
+if (process.env.NODE_ENV === "production") {
+  url = "https://reverse-ipa.herokuapp.com";
+}
+
 export const fetchResults = (term, type, position, skip) =>
-  axios.get(`/words/${term}/${type}/${position}/${skip}/40`);
+  axios.get(`${url}/words/${term}/${type}/${position}/${skip}/40`);
