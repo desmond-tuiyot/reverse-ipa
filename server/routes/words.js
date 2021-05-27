d96a8c8c83026a78f6b0fdea33e6d0fbf290d954
Desmond Tuiyot <dtuiyot@gmail.com>
2021-05-26 23:13:33 -0400

    removed server files

--- server/routes/words.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { fetchResults } from "../controllers/words.js";
-
-const router = express.Router();
-
-router.get("/results/:term/:type/:position/:skip/:limit", fetchResults);
-
-export default router;
